5f53c9b382805e0568726d4acf1abc96c71b332a
Vasylenko Dmytro <vasilenkodima292@gmail.com>
2026-03-18 23:42:23 +0200

    implement MVVM architecture and modular project structure

---
+++ client/view_model/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QString>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "ViewModel";
+
+    return a.exec();
+}
