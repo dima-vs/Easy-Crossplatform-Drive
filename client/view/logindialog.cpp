f2b319c3869f28ed8e2da54eda5ca605de49ca5f
shenzi-lee <ermolenkoolga276@gmail.com>
2026-04-15 11:43:01 +0300

    feat: add validators for email fields

--- client/view/logindialog.cpp
+++ client/view/logindialog.cpp
@@ -1,11 +1,19 @@
 #include "logindialog.h"
 #include "ui_logindialog.h"
+#include <qregularexpression.h>
+#include <qvalidator.h>
 
 logindialog::logindialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::logindialog)
 {
     ui->setupUi(this);
+
+    QString emailPattern = R"(^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$)";
+    QRegularExpression rx(emailPattern);
+    QRegularExpressionValidator *validator = new QRegularExpressionValidator(rx, this);
+
+    ui->loginEdit->setValidator(validator);
 }
 
 logindialog::~logindialog()
--- client/view/signupdialog.cpp
+++ client/view/signupdialog.cpp
@@ -1,11 +1,19 @@
 #include "signupdialog.h"
 #include "ui_signupdialog.h"
+#include <qregularexpression.h>
+#include <qvalidator.h>
 
 signupdialog::signupdialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::signupdialog)
 {
     ui->setupUi(this);
+
+    QString emailPattern = R"(^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$)";
+    QRegularExpression rx(emailPattern);
+    QRegularExpressionValidator *validator = new QRegularExpressionValidator(rx, this);
+
+    ui->loginEdit->setValidator(validator);
 }
 
 signupdialog::~signupdialog()
